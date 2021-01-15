85a98b2a067c5e4b0d97712625575fd880178181
Dhammike Piyumal <dhammikepiyumal@gmail.com>
2021-01-15 22:23:44 +0530

    Sign In, Register and Home Page

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,18 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router } from 'react-router-dom';
-import { createMemoryHistory } from 'history'
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-const history = createMemoryHistory();
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-    <Router history={history}>
-      <App />
-    </Router>,
-    div
-  );
-  ReactDOM.unmountComponentAtNode(div);
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
---
+++ client/src/components/auth/Loginpage.js
@@ -0,0 +1,58 @@
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Loginpage = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("Success");
+  };
+
+  return (
+    <section className="container">
+      <Fragment>
+        <h1 className="large text-primary">Sign In</h1>
+        <p className="lead">
+          <i className="fas fa-user"></i> Sign Into Your Account
+        </p>
+        <form className="form" onSubmit={(e) => onSubmit(e)}>
+          <div className="form-group">
+            <input
+              type="email"
+              placeholder="Email Address"
+              name="email"
+              value={email}
+              onChange={(e) => onChange(e)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={(e) => onChange(e)}
+              minLength="6"
+            />
+          </div>
+          <input type="submit" className="btn btn-primary" value="Login" />
+        </form>
+        <p className="my-1">
+          Do not have an account? <Link to="/register">Sign Up</Link>
+        </p>
+      </Fragment>
+    </section>
+  );
+};
+
+export default Loginpage;
